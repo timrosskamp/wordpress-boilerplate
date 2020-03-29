5e08ab46894f03eb788be017b3cff118e9da6e43
Tim Roßkamp <mail@timrosskamp.de>
2020-03-29 12:54:26 +0200

    JS workflow

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from "rollup-plugin-terser"
+import buble from '@rollup/plugin-buble';
+
+export default [{
+    input: 'src/js/main.js',
+    output: [{
+        file: 'dist/script.js',
+        format: 'iife',
+        sourcemap: true
+    }, {
+        file: 'dist/script.min.js',
+        format: 'iife',
+        plugins: [
+            terser()
+        ]
+    }],
+    plugins: [
+        resolve(),
+        commonjs(),
+        buble()
+    ]
+}]

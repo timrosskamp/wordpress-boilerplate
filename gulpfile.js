bcc7382161991b58ba1436269d08c76f0d8bee60
Tim Roßkamp <mail@timrosskamp.de>
2020-03-29 15:01:16 +0200

    SASS workflow

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp')
+const pump = require('pump')
+const rename = require('gulp-rename')
+const sass = require('gulp-sass')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
+
+gulp.task('sass', done => {
+	pump([
+		gulp.src(['src/scss/style.scss'], {
+			sourcemaps: true
+		}),
+		sass.sync({
+			outputStyle: 'expanded'
+		}),
+		postcss([
+			autoprefixer()
+        ]),
+		gulp.dest('dist/', {
+			sourcemaps: '.'
+		})
+	], () => {
+		pump([
+			gulp.src(['dist/style.css']),
+			postcss([
+				cssnano()
+			]),
+			rename({
+				extname: '.min.css'
+			}),
+			gulp.dest('dist/')
+		], done)
+	})
+})
+
+gulp.task('default', gulp.series('sass', () => {
+	gulp.watch(['src/scss/**'], gulp.series('sass'))
+}))
